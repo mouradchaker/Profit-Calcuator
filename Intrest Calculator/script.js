16c9261791c75819d3f708c1af9faa4ca52fbfba
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2022-10-31 22:57:44 +0100

    files

---
+++ Intrest Calculator/script.js
@@ -0,0 +1,30 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    var inputBox = document.getElementById("principal");
+    inputBox.focus();
+  } else {
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML =
+      "If you deposit <mark>" +
+      principal +
+      "</mark>,<br>at an interest rate of <mark>" +
+      rate +
+      "%</mark><br>You will receive an amount of <mark>" +
+      amount +
+      "</mark>,<br>in the year <mark>" +
+      year +
+      "</mark><br>";
+  }
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}       
+
